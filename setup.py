058f2751e036aaa03854adb52c4dcfc4b38865ba
Kanishka Gour <kanishkagour28@gmail.com>
2024-11-03 11:57:42 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.5.*',
     license='MIT',
     install_requires=[
       'numpy>=1.11.3',
